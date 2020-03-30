e5c5a7fddbf6262740d6e601beb8e6bf883ea2da
Cameron <camscott678@gmail.com>
2020-03-30 14:48:23 -0400

    Simulator setup

---
+++ simulator.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+
+class GraphManager():
+    def __init__(self):
+        pass
+
+class Simulator():
+    def __init__(self, config_filename):
+        self.parseConfig(config_filename)
+
+    def parseConfig(self, config_filename):
+        with open(config_filename) as f:
+            self.parameters = json.load(f)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", 
+        help="The configuration file to use for the simulation.",
+        required=True)
+
+    args = parser.parse_args()
+
+    sim = Simulator(args.config)
