f11f53dce5cd7b0125f051f4a42d5cabd9a2dfd5
Cameron <camscott678@gmail.com>
2020-03-30 14:59:44 -0400

    Added example config file

--- simulator.py
+++ simulator.py
@@ -1,17 +1,21 @@
 import argparse
 import json
 
+
 class GraphManager():
     def __init__(self):
         pass
+
 
 class Simulator():
     def __init__(self, config_filename):
         self.parseConfig(config_filename)
 
+
     def parseConfig(self, config_filename):
         with open(config_filename) as f:
             self.parameters = json.load(f)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
